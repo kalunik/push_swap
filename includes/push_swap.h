28252ccc95d06ee76f08e7ea5a64eae761471918
kalunik <nikitaex123@gmail.com>
2021-08-10 13:54:23 +0300

    updating error func

--- includes/push_swap.h
+++ includes/push_swap.h
@@ -1,5 +1,5 @@
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
 # include <stdarg.h>
 # include <stdio.h>
--- push_swap.c
+++ push_swap.c
@@ -1,48 +1,63 @@
 #include "includes/push_swap.h"
 
-static void	ft_errors(int argc, char **argv)
+int	ft_dublicate(int i, char **argv)
+{
+	static int		dubl[4294967295];
+	unsigned int	index;
+	int				*p;
+
+	index = 0;
+	while (index < 4294967295)
+		dubl[index++] = 0;
+	p = &dubl[2147483648];
+	if (p[ft_atoi(argv[i])])
+	{
+		ft_putstr_fd("Error\n", 1);
+		return (0);
+	}
+	else
+	{
+		p[ft_atoi(argv[i])] = 1;
+		return (1);
+	}
+	return (520);
+}
+
+void	ft_errors(int argc, char **argv)
 {
 	int	i;
 	int	j;
-	static int	args[4294967295];
-/*	unsigned int index;
-
-	int	*p;*/
+	int	ans;
 
 	i = 1;
 	j = 0;
-	/*index = 0;
-	while (index < 4294967295)
-		args[index++] = 0;
-	p = &args[2147483648];*/
 	while (i <= argc)
 	{
-		printf("%d -- %d\n", i, j);
 		while (argv[i][j])
 		{
 			if (!((argv[i][j] > '/' && argv[i][j] < ':') || argv[i][j] == '-'))
 			{
 				ft_putstr_fd("Error\n", 1);
 				exit(0);
-
 			}
 			j++;
 		}
-		//printf("%d", p[1]);
-		//ft_atoi()
+		ans = ft_dublicate(i, argv);
+		printf("ans -- %d", ans);
+		if (ans == 0)
+			exit(0);
 		i++;
 		j = 0;
 	}
 }
 
-void		push_swap(int argc, char **argv)
+void	push_swap(int argc, char **argv)
 {
 	ft_errors(argc, argv);
 }
 
-int			main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-	printf("%d\n", argc);
 	push_swap(argc, argv);
 	return (0);
 }
